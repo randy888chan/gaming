f955a817578a82b8fcfaabcfc8b881a965552c2a
lazypanda450 <120668808+lazypanda450@users.noreply.github.com>
2024-03-06 01:06:11 +0530

    Update WelcomeBanner.tsx

--- src/components/sections/Dashboard/WelcomeBanner.tsx
+++ src/components/sections/Dashboard/WelcomeBanner.tsx
@@ -3,20 +3,28 @@ import React from "react";
 
 export function WelcomeBanner() {
   return (
-    <div className="relative overflow-hidden flex flex-col items-center justify-center p-4 rounded-lg lg:grid lg:grid-cols-3 gap-4 lg:p-10 bg-gradient-to-br from-orange-300 via-pink-500 to-blue-500">
+    <div 
+      className="relative overflow-hidden flex flex-col items-center justify-center p-4 rounded-lg lg:grid lg:grid-cols-3 gap-4 lg:p-10" 
+      style={{ 
+        backgroundImage: "url('/bg.png')", 
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
       <div className="lg:col-span-2 text-center lg:text-left">
         <div className="flex items-center justify-center md:justify-start">
           <h1 className="text-2xl md:text-4xl font-bold text-white drop-shadow-md">
-            Welcome to Gamba v2
+             
           </h1>
-          <img src="/wave.gif" className="h-14" alt="Gamba Logo" />
+          <img src="/logo.svg" className="h-14" alt="Gamba Logo" />
         </div>
         <p className="mt-4 text-white drop-shadow">
-          A fair, simple, and decentralized casino on Solana.
+          Play, Hold and Stake to Earn Passive Rewards.
         </p>
       </div>
       <div className="whitespace-nowrap grid grid-cols-2 grid-rows-2 gap-2 mt-5 md:flex md:flex-col md:mt-0 md:justify-start">
-        <button
+        {/* <button
           onClick={() => window.open("https://explorer.gamba.so/create")}
           className="rounded-lg p-3 bg-white hover:bg-gray-200 hover:-translate-y-0.5 transform text-black transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
         >
@@ -41,7 +49,7 @@ export function WelcomeBanner() {
           className="rounded-lg p-3 bg-white hover:bg-gray-200 hover:-translate-y-0.5 transform text-black transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
         >
           💬 Join Discord
-        </button>
+        </button> */}
       </div>
     </div>
   );
