7ba889131ef23297633f90dad6c0d3ae1454d4fb
teyothing <humodevinfo@gmail.com>
2024-10-29 11:26:26 +0300

    Update UserButton.tsx

--- src/components/ui/UserButton.tsx
+++ src/components/ui/UserButton.tsx
@@ -93,6 +93,7 @@ export function UserButton() {
                   >
                     Support
                   </button>
+                    </div>
                 </Link>
               </div>
               <div>
